c2a257647632a2435f90367fb2a38b86e881f55c
Joe Meszar <github@outk.st>
2016-12-03 15:48:40 -0500

    Finished cs1550_getattr. returns '0' on success; otherwise -ENOENT
    Refactored to return status at the end of the method, instead of
    random places within the method.

--- src/cs1550.c
+++ src/cs1550.c
@@ -95,7 +95,7 @@ typedef struct cs1550_disk_block cs1550_disk_block;
  */
 static int cs1550_getattr(const char *path, struct stat *stbuf)
 {
-    int res = 0;
+    int status = -ENOENT;                   // default is error
 
     // hold the directory, filename, and extension (if any)
     char dir[MAX_LENGTH];
@@ -109,121 +109,100 @@ static int cs1550_getattr(const char *path, struct stat *stbuf)
         stbuf->st_mode = S_IFDIR | 0755;
         stbuf->st_nlink = 2;
 
+        status = 0;                         // SUCCESS
+
     } else {
         // make sure path is within maximum length
-        if (strlen(path) >= MAX_LENGTH) {
-            return -ENOENT;                 // BAD
-
-        // OTHER CHECKS FOR BAD PATH HERE
-
-        } else {
+        if (strlen(path) < MAX_LENGTH) {
             /*
                 Get the directory and file information
 
                 On success, the function returns the number of variables filled. 
-                In the case of an input failure before any data could be successfully read, EOF is returned.
+                In the case of an input failure before any data could be successfully read, 
+                EOF is returned.
             */
             int scan_result = sscanf(path, "/%[^/]/%[^.].%s", dir, filename, ext);
 
-            if (scan_result == 0) { return -ENOENT; }                       // nothing was filled (no dir, no filesname, no ext)
-            if (strlen(dir) > MAX_FILENAME + 1) { return -ENOENT; }         // make sure directory within max length
-            if (strlen(filename) > MAX_FILENAME + 1) { return -ENOENT; }    // make sure filename within max length
-            if (strlen(ext) > MAX_EXTENSION + 1) { return -ENOENT; }        // make sure extension within max length
-
-            // open the disk file
-            FILE *disk = fopen(DISK, "rb");                                 // open with respect to binary mode
-
-            // make sure could open the disk file
-            if (disk == NULL) {
-                return -ENOENT;
+            if ((scan_result == EOF) ||                 // EOF error
+                (scan_result == 0) ||                   // nothing was filled (no dir, no filename, no ext)
+                (strlen(dir) > MAX_FILENAME) ||         // make sure directory within max length
+                (strlen(filename) > MAX_FILENAME) ||    // make sure filename within max length
+                (strlen(ext) > MAX_EXTENSION))          // make sure extension within max length
+            { 
+                // ERROR
 
             } else {
-                cs1550_root_directory root;                                 // pointer to root of disk file
-                fread(&root, sizeof(cs1550_root_directory), 1, disk);       // put first 512bytes into root struct
 
-                // search for the directory from list of valid directories 
-                int dir_found=0, location=0;
-                for (location=0; i < root.nDirectories; location++) {
-                    if (strcmp(root.directories[location], dir) == 0) {
-                        dir_found = 1;                          // input dir was found
-                        break;
-                    }
-                }
+                // open the disk file
+                FILE *disk = fopen(DISK, "rb");             // open with respect to binary mode
 
-                // make sure the path given was valid
-                if (dir_found == 0) { 
-                    return -ENOENT;                         // dir given was not valid
+                // make sure could open the disk file
+                if (disk == NULL) {
+                    // ERROR
 
                 } else {
-                    if (scan_result == 1) {                 // only given directory
-                        // fill stbuf struct
-                        stbuf->st_mode = S_IFDIR | 0755;    // 
-                        stbuf->st_nlink = 2;                // 
-                        
-                        return 0;                           // SUCCESS 
-                        
-                    } else {
-                        // check if file exists by getting directory and looking through it
-                        int fseekret = fseek(disk, SEEK_SET, root.directories[location].nStartBlock * BLOCK_SIZE);
+                    cs1550_root_directory root;                                     // pointer to root of disk file
+                    fread(&root, sizeof(cs1550_root_directory), 1, disk);           // put first 512bytes into root struct
 
-                        if (fseekret == 0) {
-                            cs1550_directory_entry working_dir;
-                            fread(&working_dir, sizeof(cs1550_directory_entry), 1, disk);       // put first 512bytes into root struct
+                    // search for the directory from list of valid directories 
+                    int dir_found=0, location=0;
+                    for (location=0; location < root.nDirectories; location++) {    // loop through valid directories
+                        if (strcmp(root.directories[location].dname, dir) == 0) {
 
-                            // make extension NULL if blank
-                            if (scan_result == 2) { ext = '\0';}
+                            // found the directory
 
-                            // loop over files to see if it exists (fname fext)
-                            int file_found=0, file_location=0;
-                            for (file_location=0; i < working_dir.nFiles; file_location++) {
-                                if ((strcmp(working_dir.files[file_location].fname, filename) == 0) 
-                                    && (strcmp(working_dir.files[file_location].fext, ext) == 0)) {
-                                    file_found = 1;                          // input dir was found
-                                    break;
+                            // if only given a directory, return the directory info
+                            if (scan_result == 1) {
+                                // only given directory
+                                stbuf->st_mode = S_IFDIR | 0755;
+                                stbuf->st_nlink = 2;
+                                
+                                status = 0;                         // SUCCESS 
+                                
+                            } else {
+                                // path contained a filename
+
+                                // check if file exists by getting directory and looking through it
+                                // fseek returns 0 if upon success; otherwise -1
+                                int fseekret = fseek(disk, root.directories[location].nStartBlock * BLOCK_SIZE, SEEK_SET);
+
+                                if (fseekret == 0) {
+                                    cs1550_directory_entry working_dir;
+                                    fread(&working_dir, sizeof(cs1550_directory_entry), 1, disk);   // put first 512bytes into root struct
+
+                                    if (scan_result == 2) { ext[0] = '\0';}                         // make extension NULL if blank
+
+                                    // loop over files to see if file actually exists (fname fext)
+                                    int file_location;
+                                    for (file_location=0; file_location < working_dir.nFiles; file_location++) {
+                                        if ((strcmp(working_dir.files[file_location].fname, filename) == 0) 
+                                            && (strcmp(working_dir.files[file_location].fext, ext) == 0)) {
+
+                                            // found the file
+                                            stbuf->st_mode = S_IFREG | 0666; 
+                                            stbuf->st_nlink = 1;                // file links
+                                            stbuf->st_size = 0;                 // file size - make sure you replace with real size!
+                                            
+                                            status = 0;                         // SUCCESS
+                                            break;
+                                        }
+                                    }
                                 }
                             }
 
-                            if (file_found == 0) {
-                                return -ENOENT;
-                            } else {
-                                // found the file.
-                            }
-
-                            stbuf->st_mode = S_IFREG | 0666; 
-                            stbuf->st_nlink = 1;                // file links
-                            stbuf->st_size = 0;                 // file size - make sure you replace with real size!
-                            return 0;                           // SUCCESS
-                        } else {
-                            return -ENOENT;
+                            break;
                         }
                     }
+
+                    // close the disk
+                    fclose(disk);
                 }
             }
-
-            // Check if name is subdirectory
-            /* 
-                // Might want to return a structure with these fields
-                stbuf->st_mode = S_IFDIR | 0755;
-                stbuf->st_nlink = 2;
-                res = 0; // no error
-            */
-
-            // Check if name is a regular file
-            /*
-                // regular file, probably want to be read and write
-                stbuf->st_mode = S_IFREG | 0666; 
-                stbuf->st_nlink = 1; // file links
-                stbuf->st_size = 0; // file size - make sure you replace with real size!
-                res = 0; //  no error
-            */
-
-            // Else return that path doesn't exist
-            res = -ENOENT;
         }
     }
 
-    // MAKE SURE TO CLOSE THE DISK BEFORE RETURNING
-    return res;
+    // return 0 if success; otherwise -ENOENT
+    return status;
 }
 
 /* 
